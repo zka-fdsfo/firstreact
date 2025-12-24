8f946e15f8dc39f1348689300a9d59397a67dd54
zaid <164772082+zka-fdsfo@users.noreply.github.com>
2025-12-24 18:15:59 +0530

    Fix import case for Procard component

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Procard from "../components/project/procard";
+import Procard from "../components/project/Procard";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
